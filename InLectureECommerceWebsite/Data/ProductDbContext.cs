c9b288441149081106eec70271fb0918f257d9b7
Justin Burns <justinburns2008@gmail.com>
2026-02-19 15:12:46 -0800

    Create db context (#14)
    * Add Entity Framework Core packages to project
    
    * Add ProductDbContext for managing Product entities
    
    * Add connection string for local SQL Server database
    
    * Add DbContext configuration for ProductDbContext
    
    * Change Price type to decimal and update migrations

---
+++ InLectureECommerceWebsite/Data/ProductDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data;
+
+public class ProductDbContext(DbContextOptions<ProductDbContext> options) : DbContext(options)
+{
+
+    // Entities to be tracked by DbContext.
+    public DbSet<Product> Products { get; set; }
+}
