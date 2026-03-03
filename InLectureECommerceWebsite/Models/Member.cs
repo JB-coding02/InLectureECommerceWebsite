c9bdab99e7a4995502755f6bc238eaa7ecac94af
JB-coding02! <justinburns2008@gmail.com>
2026-03-02 19:16:57 -0700

    Add validation annotations for Username and Password

--- InLectureECommerceWebsite/Models/Member.cs
+++ InLectureECommerceWebsite/Models/Member.cs
@@ -17,20 +17,27 @@ public class Member
     /// Public facing Username of the member
     /// Alphanumeric characters only
     /// </summary>
+    [RegularExpression("^[a-zA-Z0-9]+$",
+        ErrorMessage = "Username must be alphanumeric only.")]
+    [StringLength(25)]
     public required string Username { get; set; }
 
     /// <summary>
     /// Email for the member
     /// </summary>
+    
     public required string email { get; set; }
 
     /// <summary>
     /// The member's password
     /// </summary>
+    [StringLength(50, MinimumLength = 6,
+        ErrorMessage = "Your password must be between 6 and 50 characters")]
     public required string password { get; set; }
 
     /// <summary>
     /// The date of birth of the member
     /// </summary>
+    
     public DateOnly DateOfBirth { get; set; }
 }
