ffc2ca6122c883c4ff53990436aa8ea9245d92a4
JB-coding02! <justinburns2008@gmail.com>
2026-03-02 19:09:06 -0700

    Add Member class for user representation in ECommerce

---
+++ InLectureECommerceWebsite/Models/Member.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models;
+
+/// <summary>
+/// Represents an individual website user.
+/// </summary>
+public class Member
+{
+    /// <summary>
+    /// Unique identifier for the member
+    /// </summary>
+    [Key]
+    public int MemberId { get; set; }
+
+    /// <summary>
+    /// Public facing Username of the member
+    /// Alphanumeric characters only
+    /// </summary>
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// Email for the member
+    /// </summary>
+    public required string email { get; set; }
+
+    /// <summary>
+    /// The member's password
+    /// </summary>
+    public required string password { get; set; }
+
+    /// <summary>
+    /// The date of birth of the member
+    /// </summary>
+    public DateOnly DateOfBirth { get; set; }
+}
